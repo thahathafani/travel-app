897f6b2d46c85d1232bf515282e3193f690b5198
thahathafani <thahafazzin@gmail.com>
2024-07-16 17:49:03 +0530

    Feature/page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <h1 className="font-bold text-teal-500">
-      hello
+    <h1 className="font-bold text-teal-500 text-2xl">
+      hello WORLD!
     </h1>
   );
 }
